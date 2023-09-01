de8bee256720031d653c8cd09b71fad0cbe4b421
Jasmine Kent <jasmine.kent02@gmail.com>
2023-09-01 15:35:54 -0500

    updates
    issues with media queries
    issues with accordion HTML
    need to add grid area for work

---
+++ css/index.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+/* accordion buttons */
+
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
